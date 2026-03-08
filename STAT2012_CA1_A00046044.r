d39e7498653e2d7bd29b81ec1e82b24aa482d7f5
𝐿𝑒𝓁𝒶𐙚 <leastan5757@gmail.com>
2026-03-08 19:54:16 +0000

    Refactor package installation and data import code

--- STAT2012_CA1_A00046044.r
+++ STAT2012_CA1_A00046044.r
@@ -1,14 +1,16 @@
-# Install needed packages
+# installs the needed packages for R
 if (!requireNamespace("tidyverse", quietly = TRUE)) install.packages("tidyverse")
 if (!requireNamespace("forecast",  quietly = TRUE)) install.packages("forecast")
 
+# loads the libraries/packages for the assignment
 library(tidyverse)
 library(forecast)
 
-# Import data from the assignment excel sheet
+# imports the data from the assignment excel sheet
 weather <- read_csv(
   "Data Athenry.csv",
   skip = 24, # ignore the first 24 lines of the excel file which are metadata
   na = c("", " ", "NA"), # treats blanks as missing values
   show_col_types = FALSE
 )
+
