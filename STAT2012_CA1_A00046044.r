0bc7b17f6f138dd1d951c77d7a30afa4471c4ab4
𝐿𝑒𝓁𝒶𐙚 <leastan5757@gmail.com>
2026-03-08 18:22:02 +0000

    Add package installation checks and reformat code
    Added package installation checks for tidyverse and forecast.

--- STAT2012_CA1_A00046044.r
+++ STAT2012_CA1_A00046044.r
@@ -1,10 +1,14 @@
+# Install needed packages
+if (!requireNamespace("tidyverse", quietly = TRUE)) install.packages("tidyverse")
+if (!requireNamespace("forecast",  quietly = TRUE)) install.packages("forecast")
+
 library(tidyverse)
 library(forecast)
 
 # Import data from the assignment excel sheet
 weather <- read_csv(
   "Data Athenry.csv",
-  skip = 24,
-  na = c("", " ", "NA"),
+  skip = 24, # ignore the first 24 lines of the excel file which are metadata
+  na = c("", " ", "NA"), # treats blanks as missing values
   show_col_types = FALSE
 )
