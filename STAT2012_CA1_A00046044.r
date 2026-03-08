88b6dbf6364db6fd4e9b64dd3869df855387ce95
𝐿𝑒𝓁𝒶𐙚 <leastan5757@gmail.com>
2026-03-08 20:27:25 +0000

    Clean and transform weather data
    # keeps only needed columns and cleans the data
    weather <- weather %>%
      select(date, maxtp, mintp, rain) %>%
      mutate(
        date = as.Date(date, format = "%d-%b-%Y"),
        maxtp = as.numeric(maxtp), 
        mintp = as.numeric(mintp),
        rain = as.numeric(rain),
        mean_temp = (maxtp + mintp) / 2, # calculates the average daily temperature
        year = as.integer(format(date, "%Y")), # extracts the year and month from each date
        month_num = as.integer(format(date, "%m")), 
        month_name = factor(month.abb[month_num], levels = month.abb)
      )

--- STAT2012_CA1_A00046044.r
+++ STAT2012_CA1_A00046044.r
@@ -14,3 +14,16 @@ weather <- read_csv(
   show_col_types = FALSE
 )
 
+# keeps only needed columns and cleans the data
+weather <- weather %>%
+  select(date, maxtp, mintp, rain) %>%
+  mutate(
+    date = as.Date(date, format = "%d-%b-%Y"),
+    maxtp = as.numeric(maxtp), 
+    mintp = as.numeric(mintp),
+    rain = as.numeric(rain),
+    mean_temp = (maxtp + mintp) / 2, # calculates the average daily temperature
+    year = as.integer(format(date, "%Y")), # extracts the year and month from each date
+    month_num = as.integer(format(date, "%m")), 
+    month_name = factor(month.abb[month_num], levels = month.abb)
+  )
